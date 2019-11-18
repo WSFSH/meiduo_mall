f69b7c40c23223c26e5fd6af79967e292058d95f
ZERO <fsh19991001@163.com>
2019-11-18 17:59:09 +0800

    配置mysql

--- mall/mall/settings.py
+++ mall/mall/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'HOST': '127.0.0.1',  # 数据库主机
+        'PORT': 3306,  # 数据库端口
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': 'python',  # 数据库用户密码
+        'NAME': 'mall'  # 数据库名字
+    },
 }
 
 
