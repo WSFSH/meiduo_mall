367e324b73cc4fec4ef5c2f6ad30e6cdd4ac9ef4
ZERO <fsh19991001@163.com>
2019-11-18 20:31:58 +0800

    实现部分注册功能

--- mall/apps/users/views.py
+++ mall/apps/users/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+import re
+
+from django import http
+from django.contrib.auth import login
+from django.db import DatabaseError
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
+
+from apps.users.models import User
 
 
 class RegisterView(View):
@@ -11,3 +19,40 @@ class RegisterView(View):
         :return: 注册界面
         """
         return render(request, 'register.html')
+    
+    def post(self, request):
+        # 获取参数
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')
+        allow = request.POST.get('allow')
+        # 校验参数
+        # 判断参数是否齐全
+        if not all([username, password, password2, mobile, allow]):
+            return http.HttpResponseBadRequest('缺少必传参数')
+        # 判断用户名是否是5-20个字符
+        if not re.match(r'^[a-zA-Z0-9_]{5,20}$', username):
+            return http.HttpResponseBadRequest('请输入5-20个字符的用户名')
+        # 判断密码是否是8-20个数字
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return http.HttpResponseBadRequest('请输入8-20位的密码')
+        # 判断两次密码是否一致
+        if password != password2:
+            return http.HttpResponseBadRequest('两次输入的密码不一致')
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseBadRequest('请输入正确的手机号码')
+        # 判断是否勾选用户协议
+        if allow != 'on':
+            return http.HttpResponseBadRequest('请勾选用户协议')
+        # 保存注册数据
+        try:
+            user = User.objects.create_user(username=username, password=password, mobile=mobile)
+        except DatabaseError:
+            return render(request, 'register.html', {'register_errmsg': '注册失败'})
+        # 实现状态保持>>>将通过认证的用户的唯一标识信息（比如：用户ID）写入到当前session会话中
+        login(request, user)
+        # 响应注册结果
+        return redirect(reverse('index:index'))
+
