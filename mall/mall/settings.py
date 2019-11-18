df6043b115be3aea0781420d0591cb92167cca24
ZERO <fsh19991001@163.com>
2019-11-18 20:22:40 +0800

    修改数据库配置&添加用户模型类

--- mall/mall/settings.py
+++ mall/mall/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'PORT': 3306,  # 数据库端口
         'USER': 'root',  # 数据库用户名
         'PASSWORD': 'python',  # 数据库用户密码
-        'NAME': 'mall'  # 数据库名字
+        'NAME': 'meiduo_mall'  # 数据库名字
     },
 }
 
@@ -149,3 +149,6 @@ CACHES = {
 }
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "session"
+
+# 指定本项目用户模型类
+AUTH_USER_MODEL = 'users.User'
