e12b345f9f1dcedf1a2fdb90749c424d2448fdc0
ZERO <fsh19991001@163.com>
2019-11-19 09:08:19 +0800

    实现这侧页面短信验证码发送

--- mall/apps/verifications/views.py
+++ mall/apps/verifications/views.py
@@ -1,9 +1,12 @@
+import random
+
 from django import http
 from django.views import View
 from django_redis import get_redis_connection
 
 from utils import constants
 from libs.captcha.captcha import captcha
+from utils.response_code import RETCODE
 
 
 class ImageCodeView(View):
@@ -24,3 +27,52 @@ class ImageCodeView(View):
 
         # 响应图片验证码
         return http.HttpResponse(image, content_type='image/jpeg')
+    
+
+class SMSCodeView(View):
+    """短信验证码"""
+
+    def get(self, request, mobile):
+        """
+        :param request: 请求对象
+        :param mobile: 手机号
+        :return: JSON
+        """
+        # 接收参数
+        image_code_client = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
+
+        # 校验参数
+        if not all([image_code_client, uuid]):
+            return http.JsonResponse({'code': RETCODE.NECESSARYPARAMERR, 'errmsg': '缺少必传参数'})
+
+        # 创建连接到redis的对象
+        redis_conn = get_redis_connection('code')
+        # 提取图形验证码
+        image_code_server = redis_conn.get('img_%s' % uuid)
+        if image_code_server is None:
+            # 图形验证码过期或者不存在
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码失效'})
+        # 删除图形验证码，避免恶意测试图形验证码
+        try:
+            redis_conn.delete('img_%s' % uuid)
+        except Exception as e:
+            print(e)
+
+        # 对比图形验证码
+        image_code_server = image_code_server.decode()  # bytes转字符串
+        if image_code_client.lower() != image_code_server.lower():  # 转小写后比较
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '输入图形验证码有误'})
+
+        # 生成短信验证码：生成6位数验证码
+        sms_code = '%06d' % random.randint(0, 999999)
+
+        # 保存短信验证码
+        redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+        # 为了在测试期间减少短信发送，可以屏蔽发送短信功能，使用打印的方式来获取到真正的验证码值
+        # 发送短信验证码>>>阿里云
+        # send_sms(mobile, sms_code)
+        print(">>>>短信验证码>>>>>>", sms_code)
+
+        # 响应结果
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信成功'})
