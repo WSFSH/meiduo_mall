ac62207a5332807cfaf3d26852e1155128fc9dfe
ZERO <fsh19991001@163.com>
2019-11-21 15:08:05 +0800

    实现收货地址删除操作

--- mall/apps/areas/views.py
+++ mall/apps/areas/views.py
@@ -205,3 +205,19 @@ class UpdateDestroyAddressView(LoginRequiredJSONMixin, View):
 
         # 响应更新地址结果
         return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '更新地址成功', 'address': address_dict})
+
+    def delete(self, request, address_id):
+        """删除地址"""
+        # 判断当前收货地址的id是否是属于当前用户
+        address = Address.objects.get(id=address_id)
+        if not (address and address.user == request.user):
+            return http.HttpResponseBadRequest('非法操作')
+    
+        try:
+            # 将地址逻辑删除设置为True
+            address.is_deleted = True
+            address.save()
+        except Exception as e:
+            return http.JsonResponse({'code': RETCODE.DBERR, 'errmsg': '删除地址失败'})
+    
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '删除地址成功'})
