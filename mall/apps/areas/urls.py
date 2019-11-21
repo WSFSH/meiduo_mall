37d4bde7f7d89eed02a3c700ab7ec5006eb46378
ZERO <fsh19991001@163.com>
2019-11-21 14:59:36 +0800

    实现用户中心-添加收货地址

--- mall/apps/areas/urls.py
+++ mall/apps/areas/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^areas/$', views.AreasView.as_view(), name='areas'),
+    url(r'^address/create/$', views.CreateAddressView.as_view(), name='createaddress'),
 ]
