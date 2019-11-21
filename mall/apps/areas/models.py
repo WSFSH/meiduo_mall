09b25a3becc8f26452bcde956b59ddbbd762a346
ZERO <fsh19991001@163.com>
2019-11-21 14:58:58 +0800

    添加&修改数据模型

--- mall/apps/areas/models.py
+++ mall/apps/areas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from apps.users.models import User
 
 '''自关联字段的外键指向自身，所以models.ForeignKey('self')
 
@@ -29,3 +30,27 @@ class Area(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+class Address(models.Model):
+    """用户地址"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='用户')
+    title = models.CharField(max_length=20, verbose_name='地址名称')
+    receiver = models.CharField(max_length=20, verbose_name='收货人')
+    # 指明外键时，可以使用应用名.模型类名来定义
+    province = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='province_addresses', verbose_name='省')
+    city = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='city_addresses', verbose_name='市')
+    district = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='district_addresses', verbose_name='区')
+    place = models.CharField(max_length=50, verbose_name='地址')
+    mobile = models.CharField(max_length=11, verbose_name='手机')
+    tel = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='固定电话')
+    email = models.CharField(max_length=30, null=True, blank=True, default='', verbose_name='电子邮箱')
+    is_deleted = models.BooleanField(default=False, verbose_name='逻辑删除')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        db_table = 'tb_address'
+        verbose_name = '用户地址'
+        verbose_name_plural = verbose_name
+        ordering = ['-update_time']
